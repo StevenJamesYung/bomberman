b1684093fb3d6bf1a04a30a82f2a8a21fc960378
Steven Yung <stevenyung@icloud.com>
2016-08-24 11:36:53 +0200

    Add binding on server's socket

--- server/server.c
+++ server/server.c
@@ -3,11 +3,19 @@
 int main(int argc, char **argv) {
   int s;
   struct protoent *pe;
+  struct sockaddr_in sin;
+  int b;
 
   pe = getprotobyname("tcp");
   s = socket(PF_INET, SOCK_STREAM, pe->p_proto);
 
-  printf("%d\n", s);
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons(4242);
+  sin.sin_addr.s_addr = INADDR_ANY;
+
+  b = bind(s, (struct sockaddr*) &sin,  sizeof(sin));
+
+  printf("%d\n", b);
 
   return 0;
 }
