064fe9feb0839d687e28a7b2ff4b8129c8876086
Steven Yung <stevenyung@icloud.com>
2016-08-24 12:07:00 +0200

    Add client/

---
+++ client/client.c
@@ -0,0 +1,16 @@
+#include "client.h"
+
+int main(int argc, char **argv) {
+  int s;
+  struct sockaddr_in sin;
+
+  s = socket(PF_INET, SOCK_STREAM, 0);
+
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons(4242);
+  sin.sin_addr.s_addr = inet_addr("localhost");
+
+  printf("%d\n", connect(s, (struct sockaddr *) &sin, sizeof(struct sockaddr_in)));
+
+  return 0;
+}
---
+++ client/client.h
@@ -0,0 +1,9 @@
+#ifndef __CLIENT_H__
+# define __CLIENT_H__
+# define _GNU_SOURCE
+
+# include "../bomberman.h"
+# include <netinet/in.h>
+# include <arpa/inet.h>
+
+#endif
