5a708e1c136df4ac0a08f521efe98d2c2bb82349
Steven Yung <stevenyung@icloud.com>
2016-09-02 14:25:15 +0200

    WIP Add attribute initialization to init_player()

--- server/player.c
+++ server/player.c
@@ -1,7 +1,12 @@
 #include "player.h"
 
-t_player *init_player() {
+t_player *init_player(int x, int y) {
   t_player *player;
   player = malloc(sizeof(t_player));
+
+  player->x = x;
+  player->y = y;
+  player->username = "okko";
+
   return player;
 }
