b567dbcd5834f14da641b5c073d3f1d0cd9eff96
traore_a <abdoulayekt@hotmail.com>
2016-09-02 21:25:02 +0200

    wip printf command.c

--- server/commands.c
+++ server/commands.c
@@ -1,18 +1,17 @@
 #include "commands.h"
 
 void up(t_global *global, int player) {
-
-    printf("UP\n", global->players[player]->username);
+    printf("UP %s\n", global->players[player]->username);
 }
 void down(t_global *global, int player) {
-    printf("DOWN\n", global->players[player]->username);
+    printf("DOWN %s\n", global->players[player]->username);
 }
 void right(t_global *global, int player) {
-    printf("RIGHT\n", global->players[player]->username);
+    printf("RIGHT %s\n", global->players[player]->username);
 }
 void left(t_global *global, int player) {
-    printf("LEFT\n", global->players[player]->username);
+    printf("LEFT %s\n", global->players[player]->username);
 }
 void drop(t_global *global, int player) {
-    printf("DROP\n", global->players[player]->username);
+    printf("DROP %s\n", global->players[player]->username);
 }
