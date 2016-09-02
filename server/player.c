76be933160f887bd41a4aa0cefc3512956453ba3
Steven Yung <stevenyung@icloud.com>
2016-09-02 14:12:57 +0200

    WIP Add player.c

---
+++ server/player.c
@@ -0,0 +1,7 @@
+#include "player.h"
+
+t_player *init_player() {
+  t_player *player;
+  player = malloc(sizeof(t_player));
+  return player;
+}
---
+++ server/player.h
@@ -0,0 +1,17 @@
+#ifndef __PLAYER_H__
+# define __PLAYER_H__
+# define _GNU_SOURCE
+
+# include "../bomberman.h"
+
+typedef struct s_player t_player;
+struct s_player {
+  int x;
+  int y;
+  char *username;
+  int socket;
+};
+
+t_player *init_player();
+
+#endif
