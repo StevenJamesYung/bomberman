759b1d2bc8c7989adda13c23ed7064c36a732362
traore_a <abdoulayekt@hotmail.com>
2016-09-05 01:56:42 +0200

    upgrade to standard server

--- server/server.h
+++ server/server.h
@@ -2,12 +2,16 @@
 # define __SERVER_H__
 # define _GNU_SOURCE
 
-# include "../bomberman.h"
-# include "global.h"
-# include "map.h"
-# include <netdb.h>
-# include <sys/select.h>
-# include "commands.h"
+#include "../bomberman.h"
+#include "global.h"
+#include "map.h"
+#include <netdb.h>
+#include <sys/select.h>
+#include "commands.h"
+#include "command_functions.h"
+
+void main_loop(int s);
+int main();
 
 # define MAX_CONNECTIONS 4
 # define PORT 4242
