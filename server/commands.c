f90286b53897eb69856863f75bccf340407819f9
traore_a <abdoulayekt@hotmail.com>
2016-09-05 21:23:36 +0200

    gestion erreur commands.c

--- server/commands.c
+++ server/commands.c
@@ -1,63 +1,78 @@
 #include "commands.h"
 
-void up(t_map *map, int player)
+int up(t_map *map, int player)
 {
   int i;
 
   i = search_player_by_socket(map->players, map->nb_players, player);
+  if (i == -1)
+    return (-1);
   if (map->players[i]->y - 1 >= 0) 
   {
     map->players[i]->y -= 1;
   }
   printf("-------------\n");
   debug_map(map);
+  return (0);
 }
 
-void down(t_map *map, int player)
+int down(t_map *map, int player)
 {
   int i;
 
   i = search_player_by_socket(map->players, map->nb_players, player);
+  if (i == -1)
+    return (-1);
   if (map->players[i]->y + 1 < HEIGHT) 
   {
     map->players[i]->y += 1;
   }
   printf("-------------\n");
   debug_map(map);
+  return (0);
 }
 
-void right(t_map *map, int player)
+int right(t_map *map, int player)
 {
   int i;
 
   i = search_player_by_socket(map->players, map->nb_players, player);
+  if (i == -1)
+    return (-1);
   if (map->players[i]->x + 1 < WIDTH) 
   {
     map->players[i]->x += 1;
   }
   printf("-------------\n");
   debug_map(map);
+  return (0);
 }
 
-void left(t_map *map, int player)
+int left(t_map *map, int player)
 {
   int i;
 
   i = search_player_by_socket(map->players, map->nb_players, player);
+  if (i == -1)
+    return (-1);
   if (map->players[i]->x - 1 >= 0) 
   {
     map->players[i]->x -= 1;
   }
   printf("-------------\n");
   debug_map(map);
+  return (0);
 }
 
-void drop(t_map *map, int player)
+int drop(t_map *map, int player)
 {
   int i;
 
   i = search_player_by_socket(map->players, map->nb_players, player);
+  if (i == -1)
+    return (-1);
   printf("DROP %d\n", player);
   printf("DROP %s\n", map->players[i]->username);
   debug_map(map);
+  return (0);
 }
