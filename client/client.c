41c4ece59251b7ac5cc7093ab4d5b15bfc722aef
traore_a <abdoulayekt@hotmail.com>
2016-09-08 19:33:15 +0200

    fix first command sent being incorrect

--- client/client.c
+++ client/client.c
@@ -19,7 +19,7 @@ void convert_signal_to_cmd(int ch, char** cmd)
 
   tmp = ch - 63 + '0';
   tmp_char = (char)tmp;
-  *cmd = &tmp_char;
+  strcpy(*cmd, &tmp_char);
 }
 
 int handle_user_input(int s)
